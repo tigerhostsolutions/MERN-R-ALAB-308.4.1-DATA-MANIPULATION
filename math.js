0ad4e36cb9c27adf08ed2eb69b3e3f7be4510bd4
TigerHost <jgrasty@tigerhost.domains>
2024-11-04 10:23:33 -0500

    Part1

--- math.js
+++ math.js
@@ -3,7 +3,7 @@ const n1 = 10;
 const n2 = 15;
 const n3 = 20;
 const n4 = 5;
-
+/*
 // Check one: add up to 50
 // This is a fairly simple operation using
 // arithmetic operators and a comparison.
@@ -43,3 +43,30 @@ const dontDoThis = ((n1 + n2 + n3 + n4) == 50) &&
   ((n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2) && 
   !(n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25) && 
   (n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4);
+*/
+
+//Check if all numbers are divisible by 5
+const is5DivisibleBy5 = (n1 % 5);
+console.log(is5DivisibleBy5);
+const is15DivisibleBy5 = (n2 % 5);
+console.log(is15DivisibleBy5);
+const is20DivisibleBy5 = (n3 % 5);
+console.log(is20DivisibleBy5);
+const is4DivisibleBy5 = (n4 % 5);
+console.log(is4DivisibleBy5);
+
+// Check if the first number is larger than the last
+const isN1GreaterThanN4 = n1 > n4;
+console.log(`It is ${isN1GreaterThanN4} that the first number is greater than the second.`);
+
+//Subtract the first number from the second number.
+const subN2MinusN1 = n2 - n1;
+console.log(subN2MinusN1);
+
+//Multiply the result by the third number.
+const resultMultipliedByN3 = subN2MinusN1 * n3;
+console.log(resultMultipliedByN3);
+
+//Find the remainder of dividing the result by the fourth number.
+const findRemainder = resultMultipliedByN3 % n4;
+console.log(findRemainder);
